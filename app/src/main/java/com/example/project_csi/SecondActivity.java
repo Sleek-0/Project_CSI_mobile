8697a290faa3ac0388525b186b1a008f6e059ff0
Hassanshadad <hshaddad400@gmail.com>
2024-05-18 04:51:55 +0300

    hello

--- app/src/main/java/com/example/project_csi/SecondActivity.java
+++ app/src/main/java/com/example/project_csi/SecondActivity.java
@@ -11,14 +11,26 @@ import com.google.android.material.navigation.NavigationView;
 
 public class SecondActivity extends AppCompatActivity {
 ImageButton show;
-BottomNavigationView bottomNavigationview;
+NavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         show=findViewById(R.id.show_bar);
+        bottomNavigationView = findViewById(R.id.bar);
 
 
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Toggle visibility of BottomNavigationView
+                if (bottomNavigationView.getVisibility() == View.VISIBLE) {
+                    bottomNavigationView.setVisibility(View.GONE);
+                } else {
+                    bottomNavigationView.setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
 
     }
